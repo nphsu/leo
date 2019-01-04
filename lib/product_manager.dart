b5446bf97ef90f08110e22f7027b0d9fdaa61b45
小池駿平 <shunpei42ba@gmail.com>
2019-01-04 17:22:26 +0100

    change to outer

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -2,29 +2,40 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct);
+
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _ProductManagerState();
   }
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food tester'];
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Container(
-      margin: EdgeInsets.all(10.0),
-      child: RaisedButton(
-        onPressed: () {
-          setState(() {
-            _products.add('Advanced Food');
-          });
-        },
-        child: Text('add product'),
+    return Column(children: [
+      Container(
+        margin: EdgeInsets.all(10.0),
+        child: RaisedButton(
+          onPressed: () {
+            setState(() {
+              _products.add('Advanced Food');
+            });
+          },
+          child: Text('add product'),
+        ),
       ),
-    ),
-    Products(_products)
+      Products(_products)
     ]);
   }
 }
