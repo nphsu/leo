22584c8f879ae9bc7670b6837ecb1e5c10868d5f
小池駿平 <shunpei42ba@gmail.com>
2019-01-04 16:55:33 +0100

    split to multi-file

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
+import './product_manager.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State {
-  List<String> _products = ['Food tester'];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,33 +11,7 @@ class _MyAppState extends State {
         appBar: AppBar(
           title: Text('data'),
         ),
-        body: Column(children: [
-          Container(
-            margin: EdgeInsets.all(10.0),
-            child: RaisedButton(
-              onPressed: () {
-                setState(() {
-                  _products.add('Advanced Food');
-                });
-              },
-              child: Text('add product'),
-            ),
-          ),
-          Column(
-            children: _products
-                .map(
-                  (element) => Card(
-                        child: Column(
-                          children: <Widget>[
-                            Image.asset('assets/2419.JPG'),
-                            Text("Food paradaice")
-                          ],
-                        ),
-                      ),
-                )
-                .toList(),
-          ),
-        ]),
+        body: ProductManager(),
       ),
     );
   }
