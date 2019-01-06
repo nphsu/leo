2fa1072493ea55da6b10ef5809704571eabe7613
小池駿平 <shunpei42ba@gmail.com>
2019-01-06 01:16:39 +0100

    add validator

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -15,56 +15,93 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   String _titleValue;
   String _descriptionValue;
   double _priceValue;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Widget _buildTitleTextField() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Product Title'),
+      autovalidate: true,
+      validator: (String value) {
+        if (value.isEmpty && value.length < 5) {
+          return 'Title is required and should be 5+ characters long.';
+        }
+      },
+      onSaved: (String value) {
+        setState(() {
+          _titleValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildTitleDescriptionField() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Product Description'),
+      validator: (String value) {
+        if (value.isEmpty && value.length < 10) {
+          return 'Description is required and should be 10+ characters long.';
+        }
+      },
+      onSaved: (String value) {
+        setState(() {
+          _descriptionValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildPriceTextField() {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(labelText: 'Product Price'),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Price is required and should be a number.';
+        }
+      },
+      onSaved: (String value) {
+        setState(() {
+          _priceValue = double.parse(value);
+        });
+      },
+    );
+  }
+
+  void _submitForm() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
+    final Map<String, dynamic> product = {
+      'title': _titleValue,
+      'description': _descriptionValue,
+      'price': _priceValue,
+      'image': 'assets/2419.JPG'
+    };
+    widget.addProduct(product);
+    Navigator.pushReplacementNamed(context, '/products');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(10.0),
-      child: ListView(
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(labelText: 'Product Title'),
-            onChanged: (String value) {
-              setState(() {
-                _titleValue = value;
-              });
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Product Description'),
-            maxLines: 4,
-            onChanged: (String value) {
-              setState(() {
-                _descriptionValue = value;
-              });
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Product Price'),
-            keyboardType: TextInputType.number,
-            onChanged: (String value) {
-              setState(() {
-                _priceValue = double.parse(value);
-              });
-            },
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          RaisedButton(
-            child: Text('Save'),
-            onPressed: () {
-              final Map<String, dynamic> product = {
-                'title': _titleValue,
-                'description': _descriptionValue,
-                'price': _priceValue,
-                'image': 'assets/2419.JPG'
-              };
-              widget.addProduct(product);
-              Navigator.pushReplacementNamed(context, '/');
-            },
-          )
-        ],
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            _buildTitleTextField(),
+            _buildTitleDescriptionField(),
+            _buildPriceTextField(),
+            SizedBox(
+              height: 10,
+            ),
+            RaisedButton(
+              child: Text('Save'),
+              onPressed: _submitForm,
+            )
+          ],
+        ),
       ),
     );
   }
